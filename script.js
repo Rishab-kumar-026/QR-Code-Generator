e443ad3296765ae0785a0a44be1ef41cbaf3ab3d
Rishab-kumar-026 <rks2034287@gmail.com>
2024-04-21 20:10:57 +0530

    QR Code Generator

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let input = document.getElementById("qr-text");
 let image = document.getElementById("qr-img");
 let image_box = document.querySelector(".image-box");
+let input_box = document.querySelector(".input-box");
+let span = document.createElement("span");
+
 function generate_QR() {
     if (input.value.length > 0) {
         image.src = "https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=" + input.value;
         image_box.classList.add("display-img");
+
+        span.innerHTML = "\u00d7";
+        input_box.appendChild(span);
+        span.addEventListener("click", remove_qr);
     }
     else {
         input.classList.add("error");
@@ -13,5 +20,12 @@ function generate_QR() {
         }, 1000);
 
     }
+}
+
+function remove_qr() {
+    image.src = '';
+    input.value = '';
+    image_box.classList.remove("display-img");
+    span.innerHTML = '';
 }
 
